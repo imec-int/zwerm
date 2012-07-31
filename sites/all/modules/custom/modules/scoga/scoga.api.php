c5c4e0c1c3456a49fab5207f2e35ef31e0903f54
Camille Reynders <camille.reynders@ibbt.be>
2012-07-31 16:04:54 +0200

    further restructuring of handlers and API

--- sites/all/modules/custom/modules/scoga/scoga.api.php
+++ sites/all/modules/custom/modules/scoga/scoga.api.php
@@ -1,10 +1,14 @@
 <?php
     /**
-     * Defines all hooks for this modules.
+     *
+     * Defines all hooks for the SCOGA module.
+     * Conforming to idiotic Drupal "best" practices this was placed in scoga.api.php
+     *
      */
 
 
     /**
+     * Called when the current campaign is switched
      * @param $new_nid
      * @param $old_nid
      */
