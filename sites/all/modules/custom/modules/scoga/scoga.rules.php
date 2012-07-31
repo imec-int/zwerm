e6289af8f05642600cb3ce19f00793455d557196
Camille Reynders <camille.reynders@ibbt.be>
2012-07-31 12:04:40 +0200

    consolidated all SCOGA modules

--- sites/all/modules/custom/modules/scoga/scoga.rules.php
+++ sites/all/modules/custom/modules/scoga/scoga.rules.php
@@ -32,21 +32,21 @@
     [approver_uid] => 0
      */
     function scoga_rule_transfer_transaction_points_to_team( $transaction ){
-        module_load_include( 'api.transaction.inc', 'scoga_core' );
+        module_load_include( 'api.transaction.inc', 'scoga' );
         scoga_transfer_transaction_points_to_team( $transaction );
     }
 
     function scoga_rule_determine_current_campaign(){
-        module_load_include( 'api.campaign.inc', 'scoga_core' );
+        module_load_include( 'api.campaign.inc', 'scoga' );
         scoga_determine_current_campaign();
     }
 
     function scoga_rule_create_campaignteam_nodes_for_campaign( $campaign_node ){
-        module_load_include( 'api.campaign.inc', 'scoga_core' );
+        module_load_include( 'api.campaign.inc', 'scoga' );
         scoga_create_campaignteam_nodes_for_campaign( $campaign_node );
     }
 
     function scoga_rule_delete_campaignteam_nodes_for_campaign( $campaign_node ){
-        module_load_include( 'api.campaign.inc', 'scoga_core' );
+        module_load_include( 'api.campaign.inc', 'scoga' );
         scoga_delete_campaignteam_nodes_for_campaign( $campaign_node );
     }
