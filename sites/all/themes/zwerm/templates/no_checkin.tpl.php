d6a4d06c046089ae26efb9d980625c8ab1fbcf09
lubrifix <tanguy.coenen@gmail.com>
2012-08-29 14:17:20 +0200

    added hyperlinks to usernames see #49

--- sites/all/themes/zwerm/templates/no_checkin.tpl.php
+++ sites/all/themes/zwerm/templates/no_checkin.tpl.php
@@ -1,5 +1,14 @@
 <?php
-    $rows = array_chunk ( $variables['content'] , 5 );
+    $names = $variables['names'];
+    $uids = $variables['uids'];
+    $formatted_names = array();
+    foreach($names as $name)
+    {
+       $element_position = array_search($name, $names);
+       $formatted_name = l($name,'user/'.$uids[$element_position ]);
+       $formatted_names[] = $formatted_name;
+    }
+    $rows = array_chunk ( $formatted_names , 5 );
     $table_variables = array(
         'rows' => $rows
     );
