553581545fe7f5d5c76a673ecb0a1f0712257243
lubrifix <tanguy.coenen@gmail.com>
2012-09-11 14:21:14 +0200

    working on zwerm theme

--- sites/all/themes/zwerm/templates/item_list.tpl.php
+++ sites/all/themes/zwerm/templates/item_list.tpl.php
@@ -1,14 +1,10 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
  * User: tanguycoenen
  * Date: 05/09/12
  * Time: 21:40
- * To change this template use File | Settings | File Templates.
  */
-    //print ('<link rel="stylesheet" type="text/css" href="'.path_to_theme().'/css/vendors/jquery.mobile-1.0.1/jquery.mobile-1.0.1.css" />');
-    //print ('<script type="text/javascript" src="'.path_to_theme().'/css/vendors/jquery.mobile-1.0.1/jquery.mobile-1.0.1.js"></script>');
-    print ('<ul data-role="listview" data-theme="g">');
+    print ('<ul data-role="listview" data-theme="c">');
     $items = $variables['items'];
     foreach ($items as $item)
     {
