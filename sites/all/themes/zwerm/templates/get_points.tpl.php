86e76dbf394ffe77cdea641f8f471305a07f3a39
lubrifix <tanguy.coenen@gmail.com>
2012-09-19 09:58:07 +0200

    outputting link to page with explanation on checkin in, added possibility to add nid to this page as a variable see #118

--- sites/all/themes/zwerm/templates/get_points.tpl.php
+++ sites/all/themes/zwerm/templates/get_points.tpl.php
@@ -1,5 +1,18 @@
 <?php
+    /*
+     * Format the incoming assignment links as links and send the output to item_list.tpl.php
+     */
     $assignment_titles = $variables['assignment_titles'];
-    $variables ['items'] = $variables['assignment_titles'];
+    $assignment_paths = $variables['assignment_paths'];
+    $i=0;
+    $assignment_links = array();
+    foreach ($assignment_titles as $title)
+    {
+        $path = $assignment_paths[$i];
+        $assignment_link = l($title,$path);
+        $assignment_links[] = $assignment_link;
+        $i = $i +1;
+    }
+    $variables ['items'] = $assignment_links;
     print theme('item_list',$variables);
 ?>
