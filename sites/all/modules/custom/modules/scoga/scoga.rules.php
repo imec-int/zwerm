86941fa5e95645d527e9228af3a94721c2b76e85
Camille Reynders <camille.reynders@ibbt.be>
2012-10-29 14:22:45 +0100

    reeneabled and fixed rules and hooks

--- sites/all/modules/custom/modules/scoga/scoga.rules.php
+++ sites/all/modules/custom/modules/scoga/scoga.rules.php
@@ -33,8 +33,8 @@
     [approver_uid] => 0
      */
     function scoga_rule_transfer_transaction_points_to_team( $transaction ){
-//        module_load_include( 'api.transaction.inc', 'scoga', 'api/scoga' );
-//        scoga_transfer_transaction_points_to_team( $transaction );
+        module_load_include( 'api.points.inc', 'scoga', 'api/scoga' );
+        scoga_transfer_transaction_points_to_team( $transaction );
     }
 
     function scoga_rule_determine_current_campaign(){
@@ -43,13 +43,13 @@
     }
 
     function scoga_rule_create_campaignteam_nodes_for_campaign( $campaign_node ){
-//        module_load_include( 'api.campaign.inc', 'scoga', 'api/scoga' );
-//        scoga_create_campaignteam_nodes_for_campaign( $campaign_node );
+        module_load_include( 'api.team.inc', 'scoga', 'api/scoga' );
+        scoga_create_campaignteam_entities_for_campaign( $campaign_node );
     }
 
     function scoga_rule_delete_campaignteam_nodes_for_campaign( $campaign_node ){
-//        module_load_include( 'api.campaign.inc', 'scoga', 'api/scoga' );
-//        scoga_delete_campaignteam_nodes_for_campaign( $campaign_node );
+        module_load_include( 'api.team.inc', 'scoga', 'api/scoga' );
+        scoga_delete_campaignteam_entities_for_campaign( $campaign_node );
     }
 
     function scoga_rule_log_user_points( $transaction ){
@@ -63,7 +63,7 @@
 //        scoga_create_message_entity( $description, t( 'Points transaction' ), $transaction->uid, $transaction->points );
     }
 
-    function scoga_rule_close_wisheditems(){
+//    function scoga_rule_close_wisheditems(){
 //        module_load_include( 'api.wisheditem.inc', 'scoga', 'api/scoga' );
 //        scoga_close_all_open_wisheditems();
-     }
+//     }
