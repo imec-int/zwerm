0cda9f32736a9067ece49ec6ab5245951f3e2b23
lubrifix <tanguy.coenen@gmail.com>
2013-01-10 16:07:55 +0100

    themed time to end of campaign see #179

--- sites/all/themes/zwerm/templates/time_interval.tpl.php
+++ sites/all/themes/zwerm/templates/time_interval.tpl.php
@@ -5,11 +5,20 @@
  * Time: 09:24
  */
 
-    //print('accessing template file');
+    print('<div class="time_to_end">');
+
     $time_interval_elements = $variables ["interval_elements"];
-    print('<div class="time_interval_label">'.t('Campaign end: ').'</div>');
-    $formatted_time = ($time_interval_elements[0].' '.t("d").' '.
-                       $time_interval_elements[1].' '.t("h").' '.
-                       $time_interval_elements[2].' '.t("m").' '.
-                       $time_interval_elements[3].' '.t("s"));
-    print('<div class="time_interval">'.$formatted_time.'</div>');
+    if(sizeof($time_interval_elements)==0)
+    {
+        print('No campaigns is currently active');
+    }
+    else
+    {
+        print('<div class="time_interval_label">'.t('Campaign end in: ').'</div>');
+        $formatted_time = ($time_interval_elements[0].' '.t("d").' '.
+                           $time_interval_elements[1].' '.t("h").' '.
+                           $time_interval_elements[2].' '.t("m").' '.
+                           $time_interval_elements[3].' '.t("s"));
+        print('<div class="time_interval">'.$formatted_time.'</div>');
+    }
+    print('</div>');
