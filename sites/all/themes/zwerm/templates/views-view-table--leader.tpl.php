5220c590aa11563f023887fd352cc9035f3107aa
lubrifix <tanguy.coenen@gmail.com>
2013-02-14 16:44:10 +0100

    fixed leaderboard

--- sites/all/themes/zwerm/templates/views-view-table--leader.tpl.php
+++ sites/all/themes/zwerm/templates/views-view-table--leader.tpl.php
@@ -34,7 +34,9 @@
         foreach ($rows as $row_count => $row): ?>
             <?php $playerRank = $row['php']; ?>
             <?php $nul_appended_playerRank =  '0'.strval($userLeaderboardPosition+1);?>
-            <?php if ($playerRank == $userLeaderboardPosition-3): ?>
+            <?php //dsm(intval($playerRank));?>
+            <?php //dsm($userLeaderboardPosition); ?>
+            <?php if ((intval($userLeaderboardPosition)>7)&&(intval($playerRank) == $userLeaderboardPosition-3)): ?>
                 <tr class="leaderboard_separator <?php print implode(' ', $row_classes[$row_count]); ?>">
                    <td colspan="4">...</td>
                 </tr>
