bc8834a3ead5c0089d9a2b3380f0bad6817ace3d
Camille Reynders <camille.reynders@iminds.be>
2013-03-07 09:57:46 +0100

    linked to local versions of jquery libs instead of CDN

--- apps/events/index.php
+++ apps/events/index.php
@@ -41,9 +41,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Zwerm Activiteit</title>
         <link type="text/css" rel="stylesheet" href="css/styles.css" media="all" />
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css">
-        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
+        <link rel="stylesheet" href="css/jquery.mobile-1.3.0.min.css">
+        <script src="js/jquery-1.9.1.min.js"></script>
+        <script src="js/jquery.mobile-1.3.0.min.js"></script>
         <script src="js/script.js"></script>
     </head>
     <body>
